5455d5aa3a742e28d3e215d225fb6cc721904f85
Gostrince <pa.cr1029@gmail.com>
2024-10-05 00:12:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Página cargada y lista');
+    const form = document.getElementById('contact-form');
+    const successMessage = document.getElementById('success-message');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Evita que el formulario se envíe
+        successMessage.classList.remove('hidden'); // Muestra el mensaje de éxito
+        form.reset(); // Resetea el formulario
+    });
 });
