064a92446d1bf47d9ce6743e175a554ed1a360a8
Gostrince <pa.cr1029@gmail.com>
2024-10-12 22:05:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contact-form');
+    const whatsappButton = document.querySelector('.whatsapp-btn'); // Seleccionar el ícono de WhatsApp
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Evita que el formulario se envíe de manera tradicional
-        
-        // Obtener los valores del formulario
-        const name = document.getElementById('name').value;
-        const message = document.getElementById('message').value;
-        
-        // Crear el mensaje para WhatsApp
-        const whatsappMessage = `Hola, mi nombre es ${name}. Quisiera saber lo siguiente: ${message}.`;
-        
-        // Reemplaza con tu número de WhatsApp (en formato internacional, sin signos + o -)
-        const whatsappNumber = "56953077043";
-        
-        // Generar la URL de WhatsApp con el mensaje
-        const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
-        
-        // Redirigir a la URL de WhatsApp
+    whatsappButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+
+        // Número de WhatsApp (en formato internacional, sin signos + o -)
+        const whatsappNumber = '56953077043';
+
+        // Generar la URL de WhatsApp
+        const whatsappURL = `https://wa.me/${whatsappNumber}`;
+
+        // Redirigir a WhatsApp
         window.open(whatsappURL, '_blank');
-        
-        // Resetea el formulario después de enviar el mensaje
-        form.reset();
     });
 });
